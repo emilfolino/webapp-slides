8648a74156fae308c8ad1c9a884a91d5e0b6a037
Emil Folino <emilfolino@gmail.com>
2019-04-02 11:25:04 +0200

    Reverted back to starting point for promises example

--- examples/kmom01-promises.js
+++ examples/kmom01-promises.js
@@ -1,12 +1,7 @@
 function slowFunction () {
-    return new Promise(function promiseHandler (resolve, reject) {
-        setTimeout(function timer() {
-            resolve("time is up");
-        }, 1000);
-    });
+    setTimeout(function timer() {
+        console.log("time is up");
+    }, 1000);
 }
 
-(async function () {
-    var message = await slowFunction();
-    console.log(message);
-})();
+slowFunction();
