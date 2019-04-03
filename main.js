474ebce8831ec506ce0de6ae1eccdf8ab2c393a6
Emil Folino <emilfolino@gmail.com>
2019-04-03 09:08:34 +0200

    Added highlightjs

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
 import slideorama from "slideorama";
+import highlightjs from "highlight.js";
+import "highlight.js/styles/monokai-sublime.css";
 
 slideorama.init("presentation", "slide-container");
+highlightjs.initHighlightingOnLoad();
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,13 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, ''),
         filename: "bundle.js"
-    }
+    },
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:['style-loader','css-loader']
+            }
+       ]
+    },
 };
